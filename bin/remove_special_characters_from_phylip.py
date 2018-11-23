9c35a071e5b01e5a8c49f8e745c526c51875bb37
Johannes Werner <johannes.werner@io-warnemuende.de>
2018-11-23 09:37:38 +0100

    corrected one letter variable in remove_special_characters_from_phylip.py

--- bin/remove_special_characters_from_phylip.py
+++ bin/remove_special_characters_from_phylip.py
@@ -7,7 +7,7 @@ This script removes the all characters from phylip that leads to problem with RA
 import re
 import sys
 
-with open(sys.argv[1]) as f:
-    for line in f:
+with open(sys.argv[1]) as phylip_file_open:
+    for line in phylip_file_open:
         print(re.sub(r"[\[\]();,:']", "", line).rstrip())
 
