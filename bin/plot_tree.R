c0f104d9a14ea9c9cd1bdb0318ecc42b18321ce3
Johannes Werner <johannes.werner@io-warnemuende.de>
2018-11-27 14:53:20 +0100

    added R script to plot the tree

---
+++ bin/plot_tree.R
@@ -0,0 +1,43 @@
+#!/usr/bin/env Rscript
+
+library(argparse)
+library(extrafont)
+library(ggtree)
+library(tidyverse)
+
+loadfonts(quiet = FALSE)
+print(fonts())
+
+parser <- ArgumentParser()
+parser$add_argument("-t", "--tree", default = NULL, dest = "tree",
+                    required = TRUE, help = "newick tree")
+parser$add_argument("-k", "--kallisto", default = NULL, dest = "kallisto",
+                    required = TRUE, help = "kallisto matrix (tab separated)")
+parser$add_argument("-o", "--output", default = NULL, dest = "output",
+                    required = TRUE, help = "output tree plot")
+args <- parser$parse_args()
+
+
+tree <- read.newick(args$tree)
+kallisto <- read_delim(args$kallisto, delim = "\t")
+
+kallisto_subset <- kallisto %>%
+  select(-c(protein_name, tpm_B8, tpm_B9, tpm_B10)) %>%
+  column_to_rownames("id") %>%
+  rename(A1 = tpm_A1,
+         A2 = tpm_A2,
+         A3 = tpm_A3,
+         A4 = tpm_A4,
+         A5 = tpm_A5,
+         A6 = tpm_A6,
+         A7 = tpm_A7)
+
+log_kallisto <- apply(kallisto_subset, c(1, 2), function(x) log10(x+1))
+
+p <- ggtree(tree) +
+  geom_treescale(y = -2, offset = 0.75, fontsize = 3) +
+  geom_tiplab(size = 2)
+
+gheatmap(p, log_kallisto, offset = 2, low = "blue", high = "red", colnames_angle = 90)
+ggsave(args$output, device = "pdf", dpi = 300)
+
